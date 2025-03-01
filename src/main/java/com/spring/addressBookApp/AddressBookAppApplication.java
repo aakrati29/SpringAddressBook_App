bd06a04cb90d643fda97a447aa3dedb1172c641c
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-03-01 15:25:01 +0530

    [Aakrati] add. UC1 done

--- src/main/java/com/spring/addressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/spring/addressBookApp/AddressBookAppApplication.java
@@ -3,11 +3,11 @@ package com.spring.addressBookApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AddressBookAppApplication {
+public class AddressBookAppApplication{
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		SpringApplication.run (AddressBookAppApplication.class, args );
 	}
-
 }
